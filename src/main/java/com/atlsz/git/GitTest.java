edd8ea062d70a28846f4cce82d08db889c5de286
lsssss <95274271+lszhe@users.noreply.github.com>
2022-08-29 20:29:20 +0800

    first commit

---
+++ src/main/java/com/atlsz/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atlsz.git;
+
+/**
+ * @author lsz
+ * @create 2022-08-29 19:45
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
