2ff7e4d1dcd44568ac6f6f1b3a4446f9877858a8
lsssss <95274271+lszhe@users.noreply.github.com>
2022-08-29 20:54:03 +0800

    second commit

--- src/main/java/com/atlsz/git/GitTest.java
+++ src/main/java/com/atlsz/git/GitTest.java
@@ -7,5 +7,6 @@ package com.atlsz.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
